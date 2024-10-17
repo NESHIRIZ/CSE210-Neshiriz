643323518abbf44493ccfd1bb8b2de007435266f
NESHIRIZ <tafadzwa2000s@gmail.com>
2024-10-17 17:42:14 +0200

    Done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Test Assignment class
+        Assignment assignment = new Assignment("Kelvin Vundla", "Division");
+        Console.WriteLine(assignment.GetSummary());
+
+        // Test MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment("Eric Nkomo", "Addition", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Test WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment("Sbonginkosi Moyo", "African History", "The Causes of the Partion of Africa ");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
-}
+};
+    

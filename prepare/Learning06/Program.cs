523d85ac02398cc74fa203ed8565ec863235a7b3
NESHIRIZ <tafadzwa2000s@gmail.com>
2024-10-25 12:24:58 +0200

    FINISHED LEARNING 06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+       List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 3, 5),
+            new Circle("Green", 2)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}, Area: {shape.GetArea():F2}");
+        }
     }
 }
+    
